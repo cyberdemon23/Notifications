b9ee64377b681ffc6380206191184f9225ca414d
Brandon Kobel <brandon.kobel@gmail.com>
2012-08-18 13:11:58 -0400

    Added the api area

---
+++ Web/Areas/Api/ApiAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using System.Web.Http;
+
+namespace Notifications.Web.Areas.Api
+{
+    public class ApiAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "api";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.Routes.MapHttpRoute(
+                "DefaultApi",
+                "api/{controller}/{id}",
+                new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
