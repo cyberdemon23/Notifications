c5d16dff3598771d7c9c3186ca7af88930d0c3cf
Brandon Kobel <brandon.kobel@gmail.com>
2012-08-26 12:18:06 -0400

    Modified the notifications javascript to extend jquery to begin separating everything

---
+++ Web/Scripts/jquery.notifications.js
@@ -0,0 +1,24 @@
+﻿(function ($) {
+    $.notifications = function (options) {
+
+        var settings = $.extend({ notify: null }, options);
+
+        var notificationHub = $.connection.notificationHub;
+
+        notificationHub.notify = function (data) {
+            $(data).each(function (index, item) {
+                if (settings.notify) {
+                    settings.notify(item);
+                }
+                notificationHub.remove(item.Id);
+            });
+        };
+
+        $.connection.hub.start()
+            .done(function () {
+            })
+            .fail(function () {
+                alert("Could not Connect!");
+            });
+    };
+})(jQuery);
