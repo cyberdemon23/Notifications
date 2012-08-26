ab626042590b163e65045c2f924723a41b2776b5
Brandon Kobel <brandon.kobel@gmail.com>
2012-08-25 22:38:34 -0400

    Began working on extracting the javascript into a separate class and abstracting it properly

---
+++ Web/Scripts/Notifications.js
@@ -0,0 +1,18 @@
+﻿function Notifications(options) {
+    var notificationHub = $.connection.notificationHub;
+
+    notificationHub.notify = function (data) {
+        $(data).each(function (index, item) {
+            $('#messages').append('<li>' + item.Message + '</li>');
+            notificationHub.remove(item.Id);
+        });
+    };
+
+    $.connection.hub.start()
+        .done(function () {
+        })
+        .fail(function () {
+            alert("Could not Connect!");
+        });
+
+}
